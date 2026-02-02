afd14c6fe600e9d03a58293d89298454598e315a
Sabbha20 <sabbha.power1@gmail.com>
2026-02-02 11:16:19 +0530

    feat: limited thinking budget

--- gemini_call.py
+++ gemini_call.py
@@ -1,18 +1,23 @@
-from google import genai
+from google.genai import Client, types
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-client = genai.Client()
+client = Client()
 
 gconfigs = {"contents":"Tell me a joke about computers. In 1-2 lines",
 "model":"gemini-2.5-flash"}
 
-input_tokens = client.models.count_tokens(**gconfigs)
-print(f"Input tokens: {input_tokens.total_tokens}, \n {input_tokens}")
+# input_tokens = client.models.count_tokens(**gconfigs)
+# print(f"Input tokens: {input_tokens.total_tokens}, \n {input_tokens}")
 
-response = client.models.generate_content(**gconfigs)
+response = client.models.generate_content(
+    **gconfigs, 
+    config=types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(thinking_budget=0)
+    )
+    )
 
 print(f"Response: {response.text}")
-print(f"\nResponse tokens: {response.usage_metadata.total_token_count}, \n{response.usage_metadata}")
+print(f"\nResponse tokens: {response.usage_metadata.total_token_count},\nCandidate tokens: {response.usage_metadata.candidates_token_count}\nThought tokens: {response.usage_metadata.thoughts_token_count}\nInput tokens: {response.usage_metadata.prompt_token_count} \n\n{response.usage_metadata}")
